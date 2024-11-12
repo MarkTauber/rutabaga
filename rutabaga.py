f8087f3a0f480f9fbb3519f9f6d9d1305b032068
MarkTauber <96663117+MarkTauber@users.noreply.github.com>
2024-11-12 10:18:57 +0300

    optimization
    Убраны лишние функции, оптимизирована работа, упрощенна генерация

--- rutabaga.py
+++ rutabaga.py
@@ -10,7 +10,8 @@ import re
 # ? разделить йотированные и обычные вариации ФИО
 #
 
-os.system("cls")
+if __name__ == '__main__':
+    os.system("cls") 
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.RawTextHelpFormatter,
@@ -42,120 +43,100 @@ parser.add_argument("-m", "--mask", type=str, required=True)
 parser.add_argument("-d", "--domain", type=str, required=False, metavar="DOMAIN")
 parser.add_argument("-o", "--output", type=str, required=False, metavar="PATH")
 parser.add_argument("-s", "--sex", type=str, choices=["m", "f"], required=False)
-parser.add_argument("-i", "--iotized", required=False, action="store_const", const=True, default=False)
+parser.add_argument(
+    "-i",
+    "--iotized",
+    required=False,
+    action="store_const",
+    const=True,
+    default=False,
+)
 parser.add_argument("-t", "--threads", type=int, default=4)
 
 args = parser.parse_args()
 time = datetime.datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
 dom = ""
 
+iotized_symbols = ["ye", "ya", "yu", "yo"]  # Йотированные буквы (Е Ё Ю Я)
+
+symbols = [
+    "a",
+    "e",
+    "r",
+    "t",
+    "y",
+    "u",
+    "i",
+    "o",
+    "p",
+    "s",
+    "d",
+    "f",
+    "g",
+    "h",
+    "j",
+    "k",
+    "l",
+    "z",
+    "c",
+    "v",
+    "b",
+    "n",
+    "m",
+    "w",
+    "q",
+]
+
+
 if args.domain:
     dom = args.domain.strip()
     if dom and "@" not in dom:
         dom = "@" + dom
 
-# Йотированные буквы (Е Ё Ю Я)
+
 if args.iotized:
-    symbols = [
-        "a",
-        "e",
-        "r",
-        "t",
-        "y",
-        "u",
-        "i",
-        "o",
-        "p",
-        "s",
-        "d",
-        "f",
-        "g",
-        "h",
-        "j",
-        "k",
-        "l",
-        "z",
-        "c",
-        "v",
-        "b",
-        "n",
-        "m",
-        "ye",
-        "ya",
-        "yu",
-        "yo",
-        "w",
-        "q",
-    ]
-else:
-    symbols = [
-        "a",
-        "e",
-        "r",
-        "t",
-        "y",
-        "u",
-        "i",
-        "o",
-        "p",
-        "s",
-        "d",
-        "f",
-        "g",
-        "h",
-        "j",
-        "k",
-        "l",
-        "z",
-        "c",
-        "v",
-        "b",
-        "n",
-        "m",
-        "w",
-        "q",
-    ]
+    symbols.extend(iotized_symbols)
 
 
 # Загружаем данные из файлов
 def load_data(sex=None):
     if sex == "m":
-        with open(".\\Data_M\\Familias_M.txt", "r") as file:
-            familias = [line.strip() for line in file]
-        with open(".\\Data_M\\Names_M.txt", "r") as file:
-            names = [line.strip() for line in file]
-        with open(".\\Data_M\\Surnames_M.txt", "r") as file:
-            surnames = [line.strip() for line in file]
+        path = ".\\Data_M\\"
     elif sex == "f":
-        with open(".\\Data_F\\Familias_F.txt", "r") as file:
+        path = ".\\Data_F\\"
+    else:
+        path = None
+
+    if path:
+        with open(path + "Familias_" + sex + ".txt", "r") as file:
             familias = [line.strip() for line in file]
-        with open(".\\Data_F\\Names_F.txt", "r") as file:
+        with open(path + "Names_" + sex + ".txt", "r") as file:
             names = [line.strip() for line in file]
-        with open(".\\Data_F\\Surnames_F.txt", "r") as file:
+        with open(path + "Surnames_" + sex + ".txt", "r") as file:
             surnames = [line.strip() for line in file]
-    else:  # Если sex не задан, используем оба пола
-        with open(".\\Data_M\\Familias_M.txt", "r") as file:
-            familias_m = [line.strip() for line in file]
-        with open(".\\Data_M\\Names_M.txt", "r") as file:
-            names_m = [line.strip() for line in file]
-        with open(".\\Data_M\\Surnames_M.txt", "r") as file:
-            surnames_m = [line.strip() for line in file]
+    else:
+        familias = []
+        names = []
+        surnames = []
+        for gender in ("M", "F"):
+            with open(f".\\Data_{gender}\\Familias_{gender}.txt", "r") as file:
+                familias.extend([line.strip() for line in file])
+            with open(f".\\Data_{gender}\\Names_{gender}.txt", "r") as file:
+                names.extend([line.strip() for line in file])
+            with open(f".\\Data_{gender}\\Surnames_{gender}.txt", "r") as file:
+                surnames.extend([line.strip() for line in file])
 
-        with open(".\\Data_F\\Familias_F.txt", "r") as file:
-            familias_f = [line.strip() for line in file]
-        with open(".\\Data_F\\Names_F.txt", "r") as file:
-            names_f = [line.strip() for line in file]
-        with open(".\\Data_F\\Surnames_F.txt", "r") as file:
-            surnames_f = [line.strip() for line in file]
-
-        familias = familias_m + familias_f
-        names = names_m + names_f
-        surnames = surnames_m + surnames_f
     return familias, names, surnames
 
 
 # Получаем шаблон от пользователя
 template = args.mask
+
+# Проверка на валидность шаблона
+if "$" not in template:
+    print("Ошибка: Шаблон должен содержать хотя бы одну переменную.")
+    exit(1)
+
 # Разбиваем шаблон на части, учитывая символы после $
 parts = re.split(r"\$([a-z]+)", template)
 
@@ -166,7 +147,12 @@ lock = threading.Lock()
 
 def generate_and_write(sex, file):
     # Создаем локальный словарь для каждого потока
-    data = {"f": [], "i": [], "o": [], "l": symbols}
+    data = {
+        "f": [],
+        "i": [],
+        "o": [],
+        "l": symbols,
+    }
     familias, names, surnames = load_data(sex)
     data["f"] = familias
     data["i"] = names
@@ -200,58 +186,19 @@ def worker(sex, file):
 
 if __name__ == "__main__":
     threads = []
-    if args.output:
-        try:
-            with open(args.output, "a") as filezx:
-                if args.sex == "m":
-                    for _ in range(args.threads):
-                        thread = threading.Thread(target=worker, args=("m", filezx))
-                        threads.append(thread)
-                        thread.start()
-                elif args.sex == "f":
-                    for _ in range(args.threads):
-                        thread = threading.Thread(target=worker, args=("f", filezx))
-                        threads.append(thread)
-                        thread.start()
-                else:  # Если sex не задан, генерируем для обоих полов
-                    for _ in range(args.threads):
-                        thread = threading.Thread(target=worker, args=("m", filezx))
-                        threads.append(thread)
-                        thread.start()
-                    for _ in range(args.threads):
-                        thread = threading.Thread(target=worker, args=("f", filezx))
-                        threads.append(thread)
-                        thread.start()
-                for thread in threads:
-                    thread.join()
-        except FileNotFoundError:
-            print(f"ERROR: cant create '{args.output}' or something.")
-        except PermissionError:
-            print(f"ERROR: No access to '{args.output}'.")
-
-    else:
-        # Цикл по всем комбинациям
-        with open(f"rutabaga_[{time}].txt", "a") as filezx:
-            if args.sex == "m":
+    output_file = args.output or f"rutabaga_[{time}].txt"
+    try:
+        with open(output_file, "a") as filezx:
+            for sex in args.sex or ["m", "f"]:
                 for _ in range(args.threads):
-                    thread = threading.Thread(target=worker, args=("m", filezx))
-                    threads.append(thread)
-                    thread.start()
-            elif args.sex == "f":
-                for _ in range(args.threads):
-                    thread = threading.Thread(target=worker, args=("f", filezx))
-                    threads.append(thread)
-                    thread.start()
-            else:  # Если sex не задан, генерируем для обоих полов
-                for _ in range(args.threads):
-                    thread = threading.Thread(target=worker, args=("m", filezx))
-                    threads.append(thread)
-                    thread.start()
-                for _ in range(args.threads):
-                    thread = threading.Thread(target=worker, args=("f", filezx))
+                    thread = threading.Thread(target=worker, args=(sex, filezx))
                     threads.append(thread)
                     thread.start()
             for thread in threads:
                 thread.join()
+    except FileNotFoundError:
+        print(f"ERROR: cant create '{output_file}' or something.")
+    except PermissionError:
+        print(f"ERROR: No access to '{output_file}'.")
 
     print(f"\nrutabaga_[{time}].txt \n\nLogins generated: {len(unique_logins)}")
